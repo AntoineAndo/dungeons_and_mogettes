452c6c13526f2f73ebea8594229d6045ba53c5e2
Alexandre <xaa-888@hotmail.fr>
2016-11-01 16:12:40 +0100

    Correct debug route

--- routes/debug.js
+++ routes/debug.js
@@ -1,6 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
 var asciiHelper = require('../utils/asciiHelper');
 
-router.get('/debug', function(req, res, next) {
+router.get('/', function(req, res, next) {
 
 	asciiHelper.getAsciiFromImage("https://upload.wikimedia.org/wikipedia/commons/a/ae/Castle_Neuschwanstein.jpg", 
 		function(data){
@@ -9,5 +12,6 @@ router.get('/debug', function(req, res, next) {
 			return res.json({image: data});
 		}
 	);
-  	
 });
+
+module.exports = router;
